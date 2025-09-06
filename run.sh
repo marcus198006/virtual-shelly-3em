ea112f00779dd7abea67066e3c13041dd307edf5
marcus198006 <154059882+marcus198006@users.noreply.github.com>
2025-09-06 12:53:19 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 cd /usr/src/app
-# Config-Werte von HA-Add-on nach env
+
+# Config-Werte aus HA Add-on UI
 export VENUS_IP="$(bashio::config 'venus_ip')"
 export EMULATION_PROTOCOL="$(bashio::config 'protocol')"
+export MQTT_USERNAME="$(bashio::config 'mqtt_username')"
+export MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
+export MQTT_HOST="$(bashio::config 'mqtt_host')"
+export MQTT_PORT="$(bashio::config 'mqtt_port')"
+
+# Topics von HA / Tasmota / Solaredge
+export TOPIC_NET="$(bashio::config 'topics.net_power_w')"
+export TOPIC_PV="$(bashio::config 'topics.pv_production_w')"
+export TOPIC_FEED="$(bashio::config 'topics.net_feed_power')"
 
 # Konfig-Datei generieren
 cat > config.yaml <<EOF
 mqtt:
-  host: 192.168.178.23
-  port: 1883
-  username: ""
-  password: ""
+  host: "${MQTT_HOST}"
+  port: ${MQTT_PORT}
+  username: "${MQTT_USERNAME}"
+  password: "${MQTT_PASSWORD}"
   topics:
-    net_power_w: "home/energy/net_power_w"
+    net_power_w: "${TOPIC_NET}"
+    pv_production_w: "${TOPIC_PV}"
+    net_feed_power: "${TOPIC_FEED}"
   formats:
     net_power_w: { json_key: "value" }
+    pv_production_w: { json_key: "value" }
+    net_feed_power: { json_key: "value" }
 
 venus:
   ip: "${VENUS_IP}"
